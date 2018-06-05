5d933c7ca8f9f787c1d953a5a131e779bfab9fd5
Daniel O'Brien <dannyk85.dob@gmail.com>
2018-06-05 13:17:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // write your code below!
+
+// function to return happy Holidays
+function happyHolidays()
+{
+  return "Happy holidays!"
+}
+
+// happy holidays function 1 overload for name
+function happyHolidaysTo(name)
+{
+  var string = "Happy holidays, "
+  return string + name
+}
+
+// happy holiday function 2 overloads for holiday, name
+function happyHolidayTo(holiday, name)
+{
+  var string = ``
+  return string
+}
